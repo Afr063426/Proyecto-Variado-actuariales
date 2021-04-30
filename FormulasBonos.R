e62a6b311111a7ad06642a68a40fdb3b7b281966
isaac cervantes artavia <saac9907@gmail.com>
2021-04-29 22:54:10 -0600

    Primera versión

---
+++ FormulasBonos.R
@@ -0,0 +1,37 @@
+# Scipt con las fórmulas
+library(lifecontingencies)
+
+
+#Casos de periodos completos
+precio_sucio<-function(C,m,r,F,M,n){
+  M*((1+r/m)^(-(n*m)))+F*C/m*annuity(r,n,0,m)
+}
+#Caso falta más de cupón
+precio_limpio<-function(C,m,r,F,M,n,i,d){
+  if (n>1){
+  precio_sucio(C,m,r,F,M,n)*(1+r)^(n-i/d)-C/m*F(d-i)/d
+  
+  }else{
+    (M+C*F/m)/(1+r/m*(d-i)/d)-  C*F/m*(d-i)/d
+    }
+}
+
+##Running yield
+rc<-function(C,P){
+  C/P*100
+  
+}
+
+
+##Función duración de Macaulay
+
+d_Macaulay<-function(P,C,r){
+  (sum(c(1:length(C))*C*(1+r)^(-c(1:length(C)))))/P
+}
+
+#Función duración modificada
+d_Modificada<-function(P,C,r){
+  -1/(1+r)*d_Macaulay(P,C,r)
+}
+
+
